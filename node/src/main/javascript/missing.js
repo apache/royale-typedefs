a2b7167a2b4b268633079cb6d9962d271fa07959
Josh Tynjala <joshtynjala@apache.org>
2016-07-20 11:21:51 -0700

    node: added some missing APIs

--- node/src/main/javascript/missing.js
+++ node/src/main/javascript/missing.js
@@ -23,3 +23,132 @@
  * @externs
  */
 
+/**
+ * @param {string} command
+ * @param {child_process.Options} options
+ * @return {Buffer|string}
+ */
+child_process.execSync;
+
+/**
+ * @param {string} file
+ * @param {Array.<string>} args
+ * @param {child_process.Options} options
+ * @return {Buffer|string}
+ */
+child_process.execFileSync;
+
+/**
+ * @param {string} p
+ * @return {boolean}
+ * @nosideeffects
+ */
+path.isAbsolute;
+
+/**
+ * @type {string}
+ */
+path.delimiter;
+
+/**
+ * @return {void}
+ * @see http://nodejs.org/api/process.html#process_process_abort
+ */
+Process.prototype.abort = function () {};
+
+/**
+ * @type {string}
+ * @see http://nodejs.org/api/process.html#process_process_arch
+ */
+Process.prototype.arch;
+
+/**
+ * @type {Array<string>}
+ * @see http://nodejs.org/api/process.html#process_process_argv
+ */
+Process.prototype.argv;
+
+/**
+ * @param {string} directory
+ * @see http://nodejs.org/api/process.html#process_process_chdir_directory
+ */
+Process.prototype.chdir = function (directory) {};
+
+/**
+ * @type {*}
+ * @see http://nodejs.org/api/process.html#process_process_config
+ */
+Process.prototype.config;
+
+/**
+ * @type {boolean}
+ * @see http://nodejs.org/api/process.html#process_process_connected
+ */
+Process.prototype.connected;
+
+/**
+ * @return {string}
+ * @see http://nodejs.org/api/process.html#process_process_cwd
+ */
+Process.prototype.cwd = function () {};
+
+/**
+ * @return {void}
+ * @see http://nodejs.org/api/process.html#process_process_disconnect
+ */
+Process.prototype.disconnect = function () {};
+
+/**
+ * @type {*}
+ * @see http://nodejs.org/api/process.html#process_process_env
+ */
+Process.prototype.env;
+
+/**
+ * @param {number=} code
+ * @see http://nodejs.org/api/process.html#process_process_exit_code
+ */
+Process.prototype.exit = function (code) {};
+
+/**
+ * @param {number} pid
+ * @param {string|number} signal
+ * @see http://nodejs.org/api/process.html#process_process_kill_pid_signal
+ */
+Process.prototype.kill = function (pid, signal) {};
+
+/**
+ * @type {string}
+ * @see http://nodejs.org/api/process.html#process_process_pid
+ */
+Process.prototype.pid;
+
+/**
+ * @type {string}
+ * @see http://nodejs.org/api/process.html#process_process_platform
+ */
+Process.prototype.platform;
+
+/**
+ * @type {*}
+ * @see http://nodejs.org/api/process.html#process_process_release
+ */
+Process.prototype.release;
+
+/**
+ * @return {number}
+ * @see http://nodejs.org/api/process.html#process_process_uptime
+ */
+Process.prototype.uptime = function () {};
+
+/**
+ * @type {string}
+ * @see http://nodejs.org/api/process.html#process_process_version
+ */
+Process.prototype.version;
+
+/**
+ * @type {*}
+ * @see http://nodejs.org/api/process.html#process_process_versions
+ */
+Process.prototype.versions;
